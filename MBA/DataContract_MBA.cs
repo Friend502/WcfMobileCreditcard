294edbc0c532770e2e98181fbfcb028a9fa67a8d
Prawate Wansingsu <prawate@paysbuy.com>
2015-02-09 18:11:10 +0700

    IT-76
    IT-76

---
+++ MBA/DataContract_MBA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace WcfConnectPaysbuy.MBA
+{
+        [DataContract]
+        public class DataContract_MBA
+        {
+            [DataMember]
+            public string tid { get; set; }
+            [DataMember]
+            public string respCode { get; set; }
+            [DataMember]
+            public string respMessage { get; set; }
+            [DataMember]
+            public string name { get; set; }
+            [DataMember]
+            public string address { get; set; }
+            [DataMember]
+            public string ref1 { get; set; }
+            [DataMember]
+            public string dueDate { get; set; }
+            [DataMember]
+            public string amount { get; set; }
+            [DataMember]
+            public string invoiceDate { get; set; }
+            [DataMember]
+            public string discountAmount { get; set; }
+            [DataMember]
+            public string refId { get; set; }
+        }
+        [DataContract]
+        public class DataContract_MBA_confirm
+        {
+            [DataMember]
+            public string tid { get; set; }
+            [DataMember]
+            public string respCode { get; set; }
+            [DataMember]
+            public string respMessage { get; set; }
+            [DataMember]
+            public string refId { get; set; }
+        }
+    
+}
---
+++ MBA/RequestData_MBA.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace WcfConnectPaysbuy.MBA
+{
+    public class RequestData_MBA
+    {
+        [MessageHeader]
+        public string secureCode;
+        public string tid;
+        public string contractAC;
+        public string email;
+        public string mobileNo;
+    }
+
+    public class ResponseData_MBA
+    {
+        [MessageBodyMember]
+        public DataContract_MBA Obj;
+    }
+    //-------------------------------------------------------------
+    public class RequestData_MBA_confirm
+    {
+        [MessageHeader]
+        public string secureCode;
+        public string tid;
+        public string refID;
+        public decimal amt;
+        public decimal fee;
+        public string confirmStatus;
+        public string cardsNo;
+        public string clientId;
+        public string origBankTrxId;
+    }
+    public class ResponseData_MBA_confirm
+    {
+        [MessageBodyMember]
+        public DataContract_MBA_confirm Obj;
+    }
+}
